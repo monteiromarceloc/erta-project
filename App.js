6dedc31966cc3fd10691042633d3030dfa2db035
Marcelo Monteiro <monteiromarceloc@gmail.com>
2022-07-20 20:21:30 -0300

    nova interface

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native';
 import axios from 'axios';
 
-const baseUrl = 'http://192.168.178.185';
+const baseUrl = 'http://192.168.18.21';
 
 export default function App() {
 
@@ -17,36 +17,64 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
 
       <Text style={styles.title}>
         Inclusion Life App
       </Text>
+      <StatusBar style="auto" />
 
       <View style={styles.buttoncontainer}>
-        <TouchableOpacity style={styles.button} onPress={onPress('F')}>
-          <Text style={styles.label} >Frente</Text>
+
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.5} onPress={onPress('F')}>
+          <Image
+            source={require('./assets/top-arrow.png')}
+            style={styles.buttonImage}
+          />
         </TouchableOpacity>
+
       </View>
 
       <View style={styles.buttoncontainer}>
-        <TouchableOpacity style={styles.button} onPress={onPress('D')}>
-          <Text style={styles.label} >Direita</Text>
+      <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.5} onPress={onPress('E')}>
+          <Image
+            source={require('./assets/left-arrow.png')}
+            style={styles.buttonImage}
+          />
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.button} onPress={onPress('S')}>
-          <Text style={styles.label}>Frear
-          </Text>
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.5} onPress={onPress('S')}>
+          <Image
+            source={require('./assets/stop.png')}
+            style={styles.buttonImageStop}
+          />
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.button} onPress={onPress('E')}>
-          <Text style={styles.label}>Esquerda</Text>
+
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.5} onPress={onPress('D')}>
+          <Image
+            source={require('./assets/right-arrow.png')}
+            style={styles.buttonImage}
+          />
         </TouchableOpacity>
+
       </View>
 
       <View style={styles.buttoncontainer}>
-        <TouchableOpacity style={styles.button} onPress={onPress('T')}>
-          <Text style={styles.label}>Trás</Text>
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.5} onPress={onPress('T')}>
+          <Image
+            source={require('./assets/down-arrow.png')}
+            style={styles.buttonImage}
+          />
         </TouchableOpacity>
       </View>
 
@@ -57,33 +85,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFF',
+    backgroundColor: '#ffd700',//'#FFFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  button: {
-    padding: 10,
-    minHeight: 100,
-    minWidth: 100,
-    backgroundColor: '#AAA',
+   button: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-    margin: 10,
-    flex: 1,
+    backgroundColor: '#FFFF',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    height: 100,
+    borderRadius: 5,
+    margin: 5,
+  },
+  buttonImage: {
+    padding: 10,
+    margin: 5,
+    height: 100,
+    width: 100,
+    resizeMode: 'stretch',
   },
   buttoncontainer: {
-    flexDirection: 'row',
-    padding: 10,
+    flexDirection: 'row'
   },
   label: {
     color: '#FFF',
-    fontWeight: "bold",
-    fontSize: 20,
   },
   title: {
-    fontSize: 20,
+    fontSize: 40,
     fontWeight: "bold",
     position: 'absolute',
     top: 60,
+  },
+  buttonImageStop: {
+    padding: 10,
+    margin: 5,
+    height: 100,
+    width: 100,
+    resizeMode: 'stretch',
+    backgroundColor: '#ff0000',
   }
 });
